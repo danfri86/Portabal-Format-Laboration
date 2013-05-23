49573dd1a3a63a1b9350c86617a280d7e2c715d1
Daniel Friberg <dfriberg86@gmail.com>
2013-05-23 12:18:26 +0200

    Fil fixar

---
+++ incl/script.js
@@ -0,0 +1,15 @@
+if (window.XMLHttpRequest) { // code for IE7+, Firefox, Chrome, Opera, Safari
+	xmlhttp=new XMLHttpRequest();  //Ajaxanrop
+} else { // code for IE6, IE5
+	xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+}
+
+xmlhttp.open("GET","G1.TCX",false);
+xmlhttp.send();
+xmlDoc=xmlhttp.responseXML;
+
+//var datum = xmlDoc.getElementsByTagName("Id")[0].childNodes[0].nodeValue;
+var datum = xmlDoc.getElementsByTagName("Id")[0].childNodes[0].nodeValue.split("T")[0];
+
+document.write("Datum: "+ datum);
+//document.write("Datum: "+ datum.substring(0,10));
