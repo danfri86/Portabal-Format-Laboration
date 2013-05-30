ee297a1c63b95a7c71cb1aec2a64a59e3a7d782f
Daniel Friberg <dfriberg86@gmail.com>
2013-05-30 14:20:33 +0200

    Bara grafen klar, men den funkar inte nu :(

--- incl/script.js
+++ incl/script.js
@@ -4,6 +4,7 @@
 window.onload = function() {
 	traningtotal();
 	karta();
+	pulsgraf();
 }
 
 /*-----------------------
@@ -89,7 +90,7 @@ function traningtotal() {
 	// Hämta distans från .tcx filen
 	var maxhastighet = xmlDoc.getElementsByTagName("MaximumSpeed")[0].childNodes[0].nodeValue;
 	
-	var maxhastighettxt = document.createTextNode(parseFloat(maxhastighet).toFixed(2)+" km/h");
+	var maxhastighettxt = document.createTextNode(parseFloat(maxhastighet*3.6).toFixed(2)+" km/h");
 	maxhastighetbox.appendChild(maxhastighettxt);
 	
 	/*
@@ -157,9 +158,155 @@ SLUT
 ------------------------*/
 
 /*-----------------------
-GRAF 1
+GRAF MED CHART.JS
+------------------------*/
+function pulsgraf() {
+	// Egna variabler
+	var puls = xmlDoc.getElementsByTagName("HeartRateBpm");
+	
+	var str = "";
+	for(var i=0; i<puls.length-1; i++) {
+		str = str+puls[i].getElementsByTagName("Value")[0].childNodes[0].nodeValue+",";
+	}
+					
+	// Kod för chart.js
+	var data = {
+		labels : ["January","February","March","April","May","June","July"],
+		datasets : [
+			{
+				fillColor : "rgba(255,10,0,0.3)",
+				strokeColor : "rgba(150,0,0,0.4)",
+				pointColor : "rgba(151,187,205,1)",
+				pointStrokeColor : "#fff",
+				/*
+				data : [
+					28,
+					48,
+					40,
+					19,
+					96,
+					27,
+					100
+				]
+				*/
+				
+				data : [
+					str
+				]
+			}
+		]
+	}
+	
+	var options = {
+	
+		//Boolean - If we show the scale above the chart data			
+		scaleOverlay : false,
+		
+		//Boolean - If we want to override with a hard coded scale
+		scaleOverride : false,
+		
+		//** Required if scaleOverride is true **
+		//Number - The number of steps in a hard coded scale
+		scaleSteps : null,
+		//Number - The value jump in the hard coded scale
+		scaleStepWidth : null,
+		//Number - The scale starting value
+		scaleStartValue : null,
+		
+		/* Egna puls-values på y-axeln. Byt ut mot ovan om det behövs
+		
+		//Boolean - If we want to override with a hard coded scale
+		scaleOverride : true,
+		
+		//** Required if scaleOverride is true **
+		//Number - The number of steps in a hard coded scale
+		scaleSteps : 15,
+		//Number - The value jump in the hard coded scale
+		scaleStepWidth : 10,
+		//Number - The scale starting value
+		scaleStartValue : 60,
+		*/
+	
+		//String - Colour of the scale line	
+		scaleLineColor : "rgba(0,0,0,.1)",
+		
+		//Number - Pixel width of the scale line	
+		scaleLineWidth : 1,
+	
+		//Boolean - Whether to show labels on the scale	
+		scaleShowLabels : true,
+		
+		//Interpolated JS string - can access value
+		scaleLabel : "<%=value%>",
+		
+		//String - Scale label font declaration for the scale label
+		scaleFontFamily : "'Arial'",
+		
+		//Number - Scale label font size in pixels	
+		scaleFontSize : 12,
+		
+		//String - Scale label font weight style	
+		scaleFontStyle : "normal",
+		
+		//String - Scale label font colour	
+		scaleFontColor : "#666",	
+		
+		///Boolean - Whether grid lines are shown across the chart
+		scaleShowGridLines : true,
+		
+		//String - Colour of the grid lines
+		scaleGridLineColor : "rgba(0,0,0,.05)",
+		
+		//Number - Width of the grid lines
+		scaleGridLineWidth : 1,	
+		
+		//Boolean - Whether the line is curved between points
+		bezierCurve : false,
+		
+		//Boolean - Whether to show a dot for each point
+		pointDot : false,
+		
+		//Number - Radius of each point dot in pixels
+		pointDotRadius : 3,
+		
+		//Number - Pixel width of point dot stroke
+		pointDotStrokeWidth : 1,
+		
+		//Boolean - Whether to show a stroke for datasets
+		datasetStroke : true,
+		
+		//Number - Pixel width of dataset stroke
+		datasetStrokeWidth : 2,
+		
+		//Boolean - Whether to fill the dataset with a colour
+		datasetFill : true,
+		
+		//Boolean - Whether to animate the chart
+		animation : false,
+	
+		//Number - Number of animation steps
+		animationSteps : 60,
+		
+		//String - Animation easing effect
+		animationEasing : "easeOutQuart",
+	
+		//Function - Fires when the animation is complete
+		onAnimationComplete : null
+		
+	}
+	
+	//Get the context of the canvas element we want to select
+	var ctx = document.getElementById("myChart").getContext("2d");
+	var myNewChart = new Chart(ctx).Line(data,options);
+}
+/*-----------------------
+SLUT
 ------------------------*/
 
+/*-----------------------
+GRAF 1
+------------------------*/
+/*
 window.addEventListener ("load", eventWindowLoaded, false);
 
 function eventWindowLoaded(){
@@ -207,5 +354,4 @@ function canvasApp (){
 	    context.stroke();
 		}						
 }
-
-
+*/
